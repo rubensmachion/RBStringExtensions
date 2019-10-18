ee4edae4cb775f00b7ea4bcfa8b5aba1e7bda881
Rubens Machion <>
2019-10-17 22:11:06 -0300

    First commit setup

---
+++ RBStringExtensions/RBStringExtensions.h
@@ -0,0 +1,19 @@
+//
+//  RBStringExtensions.h
+//  RBStringExtensions
+//
+//  Created by Rubens Machion on 17/10/19.
+//  Copyright © 2019 Rubens Machion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RBStringExtensions.
+FOUNDATION_EXPORT double RBStringExtensionsVersionNumber;
+
+//! Project version string for RBStringExtensions.
+FOUNDATION_EXPORT const unsigned char RBStringExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RBStringExtensions/PublicHeader.h>
+
+
---
+++ RBStringExtensionsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RBStringExtensionsExample
+//
+//  Created by Rubens Machion on 17/10/19.
+//  Copyright © 2019 Rubens Machion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
