e5a865c3e1879406038b5a1e5b1cf353ac1f37d3
Rubens Machion <>
2019-10-17 22:18:19 -0300

    Teste

---
+++ RBStringExtensions/StringExtension.swift
@@ -0,0 +1,17 @@
+//
+//  StringExtension.swift
+//  RBStringExtensions
+//
+//  Created by Rubens Machion on 17/10/19.
+//  Copyright © 2019 Rubens Machion. All rights reserved.
+//
+
+import Foundation
+
+
+public extension String {
+    
+    public func teste() -> String {
+        return "Teste"
+    }
+}
